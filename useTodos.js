85f6a569a7e782d4776a75ab0dab1b0d88048d7f
Eduardo Mendez <jemendez@ragasa.com.mx>
2024-12-07 20:28:01 -0600

    Primer Commit

---
+++ useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = ( initialState = []) => {
+    
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+    
+    useEffect(() => {
+        console.log(todos);
+        localStorage.setItem('todos', JSON.stringify( todos ));
+    }, [todos])
+    
+
+    const handleNewTodo = ( todo ) => {
+        console.log({ todo });
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch( action );
+    }
+
+    const handleDeleteTodo = (id) => {
+        console.log({ id });
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        console.log({ id });
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+    
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length ,
+        handleDeleteTodo,
+        handleToggleTodo,
+        handleNewTodo
+    }
+}
